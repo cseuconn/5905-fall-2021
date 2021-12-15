12b58b98d0de9f8ca5ed4680c5ecb1b1a4dbf4b0
Nachiket18 <nachideo.em@gmail.com>
2021-12-15 17:09:09 -0500

    Report / Performance Charts / Presentation

---
+++ Ramya-Nachiket/src/dask_k_means.py
@@ -0,0 +1,10 @@
+import dask_ml.datasets 
+import dask_ml.cluster 
+import matplotlib.pyplot as plt 
+X, y = dask_ml.datasets.make_blobs(n_samples=10000000, chunks=1000000, random_state=0, centers=3) 
+X = X.persist() 
+km = dask_ml.cluster.KMeans(n_clusters=3, init_max_iter=2, oversampling_factor=10) 
+km.fit(X) 
+#KMeans(init_max_iter=2, n_clusters=3, oversampling_factor=10) 
+fig, ax = plt.subplots() 
+ax.scatter(X[::10000, 0], X[::10000, 1], marker='.', c=km.labels_[::10000], cmap='viridis', alpha=0.25)
